e8aeaacbcddeecbc5a3e03125cc337a85d14da79
Eduardo Viero <duduviero@gmail.com>
2024-02-27 20:58:02 -0300

    Card dos Produtos

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'store.storeimages.cdn-apple.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,21 @@
-import { NextPage } from "next";
+import Header from "@/components/Header";
+import ProductsList from "@/components/ProductsList";
+import { ProductType, fetchProducts } from "@/services/products";
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { ReactNode } from "react";
+import { Container } from "reactstrap";
 
-const Products: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await fetchProducts()
+
+  return { props: { products } }
+}
+
+const Products: NextPage = (props: {
+  children?: ReactNode
+  products?: ProductType[]
+}) => {
   return (
     <>
       <Head>
@@ -10,9 +24,17 @@ const Products: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>
-        Nossos Produtos
-      </h1>
+      <Header />
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">
+            Nossos Produtos
+          </h1>
+
+          {<ProductsList products={props.products!} />}
+        </Container>
+      </main>
     </>
   )
 }
